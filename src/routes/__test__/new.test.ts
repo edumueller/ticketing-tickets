83169415913a4fb77fc5540b8e403a5bd2e0fc85
edumueller <eduardo.nicastro@icloud.com>
2021-08-20 21:12:17 -0300

    feat: add require user middleware from shared lib

--- src/routes/__test__/new.test.ts
+++ src/routes/__test__/new.test.ts
@@ -6,7 +6,9 @@ it("has a route handler listening to /api/tickets for post requests", async () =
 
   expect(response.status).not.toEqual(404);
 });
-it.todo("can only be accessed if the user is signed in");
+it("can only be accessed if the user is signed in", async () => {
+  await request(app).post("/api/tickets").send({}).expect(401);
+});
 it.todo("returns an error if an invalid title is provided");
 it.todo("returns an error if an invalid price is provided");
 it.todo("creates a ticket with valid inputs");
