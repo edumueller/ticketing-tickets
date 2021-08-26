62cbfe59ce548d5b941c7199dc500f9504dd94bf
edumueller <eduardo.nicastro@icloud.com>
2021-08-26 19:32:28 -0300

    feat: add event publish test

--- src/routes/__test__/new.test.ts
+++ src/routes/__test__/new.test.ts
@@ -2,7 +2,7 @@ import supertest from "supertest";
 import request from "supertest";
 import { app } from "../../app";
 import { Ticket } from "../../models/ticket";
-
+import { natsWrapper } from "../../nats-wrapper";
 it("has a route handler listening to /api/tickets for post requests", async () => {
   const response = await request(app).post("/api/tickets").send({});
 
@@ -62,4 +62,16 @@ it("creates a ticket with valid inputs", async () => {
   expect(tickets.length).toEqual(1);
   expect(tickets[0].title).toEqual(ticket.title);
   expect(tickets[0].price).toEqual(ticket.price);
+});
+
+it("publishes an event", async () => {
+  const ticket = { title: "product_1", price: 10 };
+
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send(ticket)
+    .expect(201);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
