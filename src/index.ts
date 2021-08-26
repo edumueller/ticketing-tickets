89ebd6a32e36d0b4a0b9fac2b0674100f0ed20a0
edumueller <eduardo.nicastro@icloud.com>
2021-08-26 17:52:05 -0300

    connect to nats when server comes up

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import { app } from "./app";
+import { natsWrapper } from "./nats-wrapper";
 
 const PORT = 3000;
 
@@ -11,6 +12,7 @@ const start = async () => {
     throw new Error("MONGO_URI must be defined");
   }
   try {
+    await natsWrapper.connect("ticketing", "lafasda", "http://nats-srv:4222");
     await mongoose.connect(process.env.MONGO_URI!, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
