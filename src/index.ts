3cb5c7ac81a692c434225892e694a18d56cf45df
edumueller <eduardo.nicastro@icloud.com>
2021-09-02 21:13:39 -0300

    add mongoose options

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,10 @@ const start = async () => {
     new OrderCreatedListener(natsWrapper.client).listen();
     new OrderCancelledListener(natsWrapper.client).listen();
 
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log('Connected to MongoDb');
   } catch (err) {
     console.error(err);
